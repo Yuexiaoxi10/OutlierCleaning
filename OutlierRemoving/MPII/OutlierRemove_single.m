df89164f1efcdafbc56657a384f841bb66804f9c
Yuexiaoxi10 <zhang.yuexi10@gmail.com>
2017-11-16 14:35:00 -0500

    update

--- OutlierRemoving/MPII/OutlierRemove_single.m
+++ OutlierRemoving/MPII/OutlierRemove_single.m
@@ -1,10 +1,11 @@
 %clear all
+dbstop if error
 %fileRoot = '/Users/zhangyuexi/Documents/LabLife/Reaserch/SpringforPhD/CPM/VideoResults/';
 fileRoot = '/Users/yuexizhang/Documents/CPM/VideoResults/';
 Batch = 1;
 filePath = [fileRoot,'Batch',num2str(Batch),'/'];
 %fileName = fullfile(filePath,['Results_Batch',num2str(Batch),'.mat']);
-fileName = fullfile(filePath,'video_singleOnly_Result.mat');
+fileName = fullfile(filePath,'video_track_center_pose.mat');
 load(fileName);
 addpath(genpath('../../3rdParty'));
 addpath(genpath('../../OutlierRemoving'));
@@ -15,7 +16,7 @@ clear  NewTracj
 order = 2;
 lambda = 1;
 
-for Nvideo = 15 %: length(video_single)
+for Nvideo = 1 %: length(video_single)
     videoPrediction = video_singleOnly_Result(Nvideo).prediction;
    %videoPrediction = video_single(Nvideo).Results;
    np = size(videoPrediction{1,1},1);
@@ -62,11 +63,54 @@ end
 end
 %
 %% Visualization
+close all
 %Picroot = '/Users/zhangyuexi/Documents/LabLife/Reaserch/SpringforPhD/MPII_videos/Batch1_single/';
 Picroot = '/Users/yuexizhang/Documents/Realtime/Video/';
 PicPath = [Picroot,num2str(Batch),'/'];
 close all
+root = '/media/yuexi/DATA/MPII_data/1/';
+%load('../VideoResults/Batch1/video_singleOnly_test.mat');
+load('/home/yuexi/Documents/CPM/VideoResults/Batch1/video_track_center_pose.mat', 'video_track')
+%load('/home/yuexi/Documents/CPM/VideoResults/Batch1/video_track_center.mat', 'video_track')
+%imPath = '/Users/yuexizhang/Documents/Realtime/Video/1/000919705';
+param = config();
+c = hsv(20);
+
 %dbstop if error
+for vid = 7 %1 : 30
+%imPath = [root,video_singleMulti_Result(vid).vidName]; 
+imPath = [root,video_track(vid).vidName];
+imlist = dir(fullfile(imPath,'*.jpg'));
+addpath(genpath('testing'));
+%load('../VideoResults/test.mat');
+predict = video_track(vid).prediction;
+
+ %predict = video_singleMulti_Result(vid).prediction;
+    for i = 1 : length(imlist)
+        
+        hold off 
+
+        test_image = [imlist(i).folder,'/',imlist(i).name];
+        imshow(imread(test_image))
+        title([num2str(i),'/',num2str(length(imlist))]) 
+        hold on
+
+        for np = 1 : length(predict) %number of person
+            videoPrediction = predict(np).pred;
+            visualizeSkeleton(videoPrediction{1,i}, param);
+            mn = min(videoPrediction{1,i});
+            mx = max(videoPrediction{1,i});
+            rect = [mn,mx-mn+1];
+            rectangle('Position',rect, 'EdgeColor',c(mod(np,20)+1,:), 'LineWidth',3);
+            %pause(0.3);
+        end 
+        pause(0.3);
+    end 
+%my_frame2video(MFV_test,1,'video');
+end
+
+%%
+
 % for vid = 1 : 10
 imPath = [PicPath,video_singleOnly_Result(Nvideo).vidName]; 
 imlist = dir(fullfile(imPath,'*.jpg'));
